74ba7b36ba39e3147fc7c367d79777bb990f0596
TungDT <dothedung131@gmail.com>
2025-11-22 00:59:21 +0700

    T1-Build OpenAPI With Swagger TO Generate Model And Controller

---
+++ src/main/java/com/apimodern/ApiDesignModern/ApiDesignModernApplication.java
@@ -0,0 +1,15 @@
+package com.apimodern.ApiDesignModern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesignModernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesignModernApplication.class, args);
+        System.out.println("API Design Modern Application is running...");
+
+	}
+
+}
---
+++ src/test/java/com/apimodern/ApiDesignModern/ApiDesignModernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimodern.ApiDesignModern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesignModernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
